0ce56b9207318031fbf1fa584c0dfcfc4d19f898
jtf <wwcxmry@163.com>
2018-11-07 13:49:51 +0800

    Initial application generated by JHipster-4.11.1

---
+++ src/main/java/com/test/service/config/FeignConfiguration.java
@@ -0,0 +1,10 @@
+package com.test.service.config;
+
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients(basePackages = "com.test.service")
+public class FeignConfiguration {
+
+}
